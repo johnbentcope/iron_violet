39ec055489294137f6a314d451c641a05e8d833f
John Cope <johnbentcope@gmail.com>
2024-02-08 21:02:10 -0500

    maybe an oscillator

---
+++ src/oscillator.v
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`define default_netname none
+
+module oscillator(){
+  input  wire       clk,    // Clock - 50MHz
+  input  wire       rst_n,  // Reset_n - active low
+  input  wire       load,   // Reset_n - active low
+  input  wire [4:0] note,   // Note selection, maybe needs more bits
+  output reg        speaker // Oscillator output
+};
+
+reg [15: 0] oscillator_counter;
+
+always @(posedge clk) begin
+
+  if(!rst_n) begin
+    speaker             <=  0;
+    oscillator_counter  <= '0;
+  end
+
+  else begin
+    oscillator_counter  <= oscillator_counter + 1;
+  end
+
+end
+
+endmodule : oscillator
--- src/project.v
+++ src/project.v
@@ -11,9 +11,9 @@ module tt_um_example (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena,      // Will go high when the design is enabled
+    input  wire       clk,      // Clock
+    input  wire       rst_n     // Reset_n - active low
 );
 
   // All output pins must be assigned. If not used, assign to 0.
