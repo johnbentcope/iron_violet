9a31d38ecf4a6f9e64b2377c1b88e00317efdf30
John Cope <johnbentcope@gmail.com>
2024-02-10 13:12:42 -0500

    yosys doesn't like undriven stuffs

--- src/project.v
+++ src/project.v
@@ -18,8 +18,9 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[5:2]  = 0;
+  assign uio_out      = 0;
+  assign uio_oe       = 0;
 
   stack #() stack_i (
     .CLK(clk),
