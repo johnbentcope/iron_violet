4b67d3eb0096419767e0a106a05bb896dc9cd8e5
John Cope <johnbentcope@gmail.com>
2024-02-10 20:57:28 -0500

    fixin build

--- test/tb.v
+++ test/tb.v
@@ -33,7 +33,7 @@ module tb ();
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      .ena    (en),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
