4e49cfd64af946e64165e346b37defef95f204ea
Kasey Hill <47794776+KaseyHDL@users.noreply.github.com>
2024-02-11 21:40:19 -0500

    Updated dff.v to use mux for synchronous resetting

--- src/dff.v
+++ src/dff.v
@@ -18,17 +18,26 @@ module dff (
     output reg  q_bar  // data out, inverted
 );
     // internal wires
+    wire d_int;
     wire w_nand_0;
     wire w_nand_1;
     wire w_nand_2;
     wire w_nand_3;
 
+    // synchronous reset
+    mux mux_reset (
+        .a (1'b0),
+        .b (d),
+        .s (rst_n),
+        .y (d_int)
+    );
+    
     // gate logic
     nand nand_0 (w_nand_0, w_nand_3, w_nand_1);
     nand nand_1 (w_nand_1, w_nand_0, rst_n, clk);
     nand nand_2 (w_nand_2, w_nand_1, clk, w_nand_3);
-    nand nand_3 (w_nand_3, w_nand_2, d, rst_n);
+    nand nand_3 (w_nand_3, w_nand_2, d_int);
     nand nand_4 (q, w_nand_1, q_bar);   
-    nand nand_5 (q_bar, q, w_nand_2, rst_n);     
+    nand nand_5 (q_bar, q, w_nand_2);     
 
 endmodule
