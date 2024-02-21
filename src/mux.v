10813546ce283a624ee22b18d796347eccb1fb2e
Kasey Hill <47794776+KaseyHDL@users.noreply.github.com>
2024-02-20 19:41:25 -0500

    Update mux.v

--- src/mux.v
+++ src/mux.v
@@ -1,21 +1,25 @@
-module mux (
+module mux #(parameter BUS_WIDTH = 1)(
   // inputs
-  input  wire a, // input a
-  input  wire b, // input b
-  input  wire s, // select
+  input  wire [BUS_WIDTH-1:0] a, // input a
+  input  wire [BUS_WIDTH-1:0] b, // input b
+  input  wire                 s, // select
   // outputs
-  output reg  y
+  output reg  [BUS_WIDTH-1:0] y
 );
   // wires
   wire s_bar;
-  wire w0;
-  wire w1;
+  wire [BUS_WIDTH-1:0] w0;
+  wire [BUS_WIDTH-1:0] w1;
 
   // gates
   not not_s (s_bar, s);
 
-  and and_w0 (w0, a, s_bar);
-  and and_w1 (w1, b, s);
-  or  or_y   (y, w0, w1);
+  generate
+    for (genvar i = 0; i < BUS_WIDTH; i = i + 1) begin
+      and and_w0_u (w0[i], a[i], s_bar);
+      and and_w1_u (w1[i], b[i], s);
+      or  or_y_u   (y[i], w0[i], w1[i]);
+    end
+  endgenerate
 
 endmodule : mux
