eb1e6c1a4c1d61863839e2ff312f1c6ab5f1c544
John Cope <johnbentcope@gmail.com>
2024-03-03 20:03:36 -0500

    maybe

--- src/project.v
+++ src/project.v
@@ -24,7 +24,6 @@ module tt_um_iron_violet_simon
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uo_out[7:5]  = 0;
   assign uio_out      = 0;
   assign uio_oe       = 0;
--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+from cocotb.triggers import FallingEdge, RisingEdge
 
 @cocotb.test()
 async def test_adder(dut):
@@ -15,47 +16,55 @@ async def test_adder(dut):
 
   # Reset
   dut._log.info("Reset")
-  dut.data_in.value = 0
-  dut.rst_n.value = 0
-  dut.push.value = 0
-  dut.pop.value = 0
+  dut.ui_in     .value = 0
+  dut.uio_in    .value = 0
+  dut.ena       .value = 0
+  dut.clk       .value = 0
+  dut.rst_n     .value = 0
+  dut.butt_red  .value = 0
+  dut.butt_yel  .value = 0
+  dut.butt_grn  .value = 0
+  dut.butt_blu  .value = 0
+  dut.butt_start.value = 0
+
+  await ClockCycles(dut.clk, 5)
+
+  dut.rst_n .value = 1
+
+  # Set the input values, wait one clock cycle, and check the output
+  dut._log.info("Test")
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_red .value = 1
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_red .value = 0
+  dut.butt_yel .value = 1
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_yel .value = 0
+  dut.butt_grn .value = 1
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_grn .value = 0
+  dut.butt_blu .value = 1
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_blu .value = 0
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_start.value = 1
+
+  await ClockCycles(dut.clk, 1)
+
+  dut.butt_start.value = 0
+
   await ClockCycles(dut.clk, 10)
-  dut.rst_n.value = 1
-
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test 1")
-
-  for a in range(5):
-    for _ in range(4):
-      dut.data_in.value = _
-      dut.push.value = 1
-      await ClockCycles(dut.clk, 1)
-      dut.push.value = 0
-      await ClockCycles(dut.clk, 1)
-
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test 2")
-
-  for a in range(5):
-    for _ in range(4):
-      dut.pop.value = 1
-      await ClockCycles(dut.clk, 1)
-      dut.pop.value = 0
-      await ClockCycles(dut.clk, 1)
-
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test 3")
-
-  for a in range(5):
-    for _ in range(4):
-      dut.data_in.value = _
-      dut.push.value = 1
-      await ClockCycles(dut.clk, 1)
-      dut.push.value = 0
-      await ClockCycles(dut.clk, 1)
-
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test 4")
 
   assert True
-  # assert dut.uo_out.value == 50
