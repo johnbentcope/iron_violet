8f78b799abfa6befbb7fe47bea2c341c25dda6a9
Kasey Hill <47794776+KaseyHDL@users.noreply.github.com>
2024-03-31 21:55:40 -0400

    Update timer.v formatting manually

--- src/timer.v
+++ src/timer.v
@@ -1,54 +1,58 @@
-`default_nettype none
+/*
+ * Copyright (c) 2023 Iron Violet LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-module timer(
-input  CLK,
-input  RST_N,
-input  START_TMR,
-output PULSE
-);
+//============================================================================//
+// Timer
+//============================================================================//
+`default_nettype none
 `include "constants.vh"
 
-reg [1:0]  state;
-reg pulse_i;
-reg [20:0] counter;
+module timer (
+  input  wire CLK,
+  input  wire RST_N,
+  input  wire START_TMR,
+  output reg  PULSE
+);
+  // internal signals
+  reg [1:0]  state;
+  reg        pulse_i;
+  reg [20:0] counter;
 
-assign PULSE = pulse_i;
+  assign PULSE = pulse_i;
 
-always @(posedge CLK or negedge RST_N) begin
+  always @(posedge CLK or negedge RST_N) begin
+    if (!RST_N) begin
+      state <= 0;
+      pulse_i       <= 0;
+      counter       <= 0;
+    end else begin
+      // Pulsed signal default values
+      pulse_i   <= 0;
 
-  // Handle reset.
-  if(!RST_N) begin
-    state <= 0;
-    pulse_i       <= 0;
-    counter       <= 0;
-  end
-  else begin
-    // Pulsed signal default values
-    pulse_i   <= 0;
-    
-    case(state)
-      TIMR_IDLE_S: begin
-        if (START_TMR) begin
-          state <= TIMR_COUNT_S;
-          counter       <= counter + 1;
+      case (state)
+        TIMR_IDLE_S: begin
+          if (START_TMR) begin
+            state   <= TIMR_COUNT_S;
+            counter <= counter + 1;
+          end
         end
-      end
-      TIMR_COUNT_S: begin
-        counter         <= counter + 1;
-        if (counter == TIMR_MAX_C) begin
-          counter       <= '0;
-          pulse_i       <=  1;
+
+        TIMR_COUNT_S: begin
+          counter   <= counter + 1;
+          if (counter == TIMR_MAX_C) begin
+            counter <= 0;
+            pulse_i <= 1;
+            state   <= TIMR_IDLE_S;
+          end
+        end
+
+        default : begin
           state <= TIMR_IDLE_S;
         end
-      end
-      default : begin
-        state <= TIMR_IDLE_S;
-      end
-
-    endcase
+      endcase
+    end
   end
-
-end
-
 
 endmodule : timer
