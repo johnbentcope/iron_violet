8375949b93de5e34507e1d387e435a59b0c986aa
Kasey Hill <kasey.fpga@gmail.com>
2024-04-17 21:39:24 -0400

    finished oscillator

--- src/oscillator.v
+++ src/oscillator.v
@@ -10,54 +10,69 @@
 `define default_netname none
 
 module oscillator (
-  input  wire       CLK,      // Clock - 10KHz
+  input  wire       CLK,      // Clock - 50MHz
   input  wire       RST_N,    // Reset_n - active low
+  input  wire       EN,       // Enable
   input  wire [1:0] NOTE_SEL, // Note selection
   output reg        AUDIO     // Oscillator output
 );
+  //-----------------------------
+  // note:        Hz : Cyc@50Mhz
+  // F#5 :  739.99Hz :    67_568
+  // A 5 :  880.00Hz :    56_818
+  // C#6 : 1108.70Hz :    45_098
+  // E 6 : 1318.50Hz :    37_922
+  //-----------------------------
 
-  //-------------------------------------------------------------------
-  // note:        Hz : Cyc@10Mhz : Cyc@50Mhz
-  // F#5 :  739.99Hz :   13_514  :    67_568
-  // A 5 :  880.00Hz :   11_363  :    56_818
-  // C#6 : 1108.70Hz :    9_020  :    45_098
-  // E 6 : 1318.50Hz :    7_584  :    37_922
-  //-------------------------------------------------------------------
+  wire clk_fsharp5;
+  wire clk_a5;
+  wire clk_csharp6;
+  wire clk_e6;
 
-  // Frequencies in millihertz
-  // localparam Fs5_f =  739_990;
-  // localparam A_5_f =  880_000;
-  // localparam Cs6_f = 1108_700;
-  // localparam E_6_f = 1318_500;
+  clk_div #(
+    .FREQ_IN  (50_000_000),
+    .FREQ_OUT (67_568)
+  ) clk_div_fsharp5 (
+    .CLK      (CLK),
+    .RST_N    (RST_N),
+    .CLK_OUT  (clk_fsharp5)
+  );
 
-  reg [ 1: 0] current_note;             // 
-  reg [13: 0] oscillator_counter;       // 
-  reg [13: 0] counter_compares   [3:0]; // this seems expensive... optimize?
+  clk_div #(
+    .FREQ_IN  (50_000_000),
+    .FREQ_OUT (56_818)
+  ) clk_div_a5 (
+    .CLK      (CLK),
+    .RST_N    (RST_N),
+    .CLK_OUT  (clk_a5)
+  );
 
-  initial begin
-    counter_compares[0] = 13_514;
-    counter_compares[1] = 11_363;
-    counter_compares[2] =  9_020;
-    counter_compares[3] =  7_584;
-  end
+  clk_div #(
+    .FREQ_IN  (50_000_000),
+    .FREQ_OUT (45_098)
+  ) clk_div_csharp6 (
+    .CLK      (CLK),
+    .RST_N    (RST_N),
+    .CLK_OUT  (clk_csharp6)
+  );
 
-  //-------------------------------------------------------------------
-  // Okay so the state machine should never introduce a high frequency
-  // runt pulse in the square wave generator.
-  // Only at when toggling the output should a new counter value be loaded.
-  //-------------------------------------------------------------------
-  always @(posedge CLK or negedge RST_N) begin
-    if (!RST_N) begin
-      AUDIO               <= 0;
-      oscillator_counter  <= 0;
-      current_note        <= NOTE_SEL;
-    end else begin
-      oscillator_counter  <= oscillator_counter + 1;
-      if (oscillator_counter == counter_compares[current_note]) begin
-        current_note      <= NOTE_SEL;
-        AUDIO             <= !AUDIO;
-      end
-    end
+  clk_div #(
+    .FREQ_IN  (50_000_000),
+    .FREQ_OUT (37_922)
+  ) clk_div_e6 (
+    .CLK      (CLK),
+    .RST_N    (RST_N),
+    .CLK_OUT  (clk_e6)
+  );
+
+  always @(*) begin
+    case (NOTE_SEL)
+      2'b0    : AUDIO = EN ? clk_fsharp5 : 0; 
+      2'b1    : AUDIO = EN ? clk_a5      : 0;
+      2'b2    : AUDIO = EN ? clk_csharp6 : 0;
+      2'b3    : AUDIO = EN ? clk_e6      : 0;
+      default : AUDIO = 0; 
+    endcase
   end
 
 endmodule : oscillator
